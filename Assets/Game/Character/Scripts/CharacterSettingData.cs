ee8eb181f48e62fcaa5ea490be433eeba44228f3
Fonarev <135630496+Fonarev@users.noreply.github.com>
2025-10-22 10:19:22 +0400

    New

---
+++ Assets/Game/Character/Scripts/CharacterSettingData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Character.Scripts
+{
+    [CreateAssetMenu(fileName ="character",menuName ="test")]
+    public class CharacterSettingData : ScriptableObject
+    {
+        [SerializeField, Range(0.5f, 10)] private float speed;
+        [SerializeField, Range(0.5f, 10)] private float jumpHeight;
+        [SerializeField, Range(0.5f, 10)] private float jumpSpeed;
+        [SerializeField, Range(0.5f, 10)] private float lookSpeed;
+        [SerializeField, Range(10, 120)] private float lookLimit;
+        [SerializeField, Range(1, 10)] private float returnSpeedCamera;
+        [SerializeField, Range(8, 22)] private float gravity;
+        public float Speed => speed;
+        public float JumpHeight => jumpHeight;
+        public float JumpSpeed => jumpSpeed;
+        public float LookSpeed => lookSpeed;
+        public float LookLimit => lookLimit;
+        public float ReturnSpeedCamera => returnSpeedCamera;
+        public float Gravity => gravity;
+    }
+}
---
+++ Assets/Game/Character/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Character.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private CharacterSettingData data;
+        [SerializeField] private bool canMove = true;
+       
+        private bool Moving;
+        private float vertical;
+        private float horizontal;
+        private float Lookvertical;
+        private float Lookhorizontal;
+        private float rotationX = 0;
+        private float InstallFOV;
+        private Vector3 moveDirection = Vector3.zero;
+        private CharacterController characterController;
+        private Camera cam;
+
+        private void Start()
+        {
+            characterController = GetComponent<CharacterController>();
+            cam = GetComponentInChildren<Camera>();
+            InstallFOV = cam.fieldOfView;
+        }
+
+        private void Update()
+        {
+            if (!characterController.isGrounded)
+                moveDirection.y -= data.Gravity * Time.deltaTime;
+
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+
+            vertical = canMove ? data.Speed * Input.GetAxis("Vertical") : 0;
+            horizontal = canMove ? data.Speed * Input.GetAxis("Horizontal") : 0;
+
+            float movementDirectionY = moveDirection.y;
+
+            moveDirection = (forward * vertical) + (right * horizontal);
+
+            if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+                moveDirection.y = data.JumpHeight;
+            else
+                moveDirection.y = movementDirectionY;
+
+            characterController.Move(moveDirection * Time.deltaTime);
+            Moving = horizontal < 0 || vertical < 0 || horizontal > 0 || vertical > 0 ? true : false;
+
+            
+                Lookvertical = -Input.GetAxis("Mouse Y");
+                Lookhorizontal = Input.GetAxis("Mouse X");
+
+                rotationX += Lookvertical * data.LookSpeed;
+                rotationX = Mathf.Clamp(rotationX, -data.LookLimit, data.LookLimit);
+                cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+                transform.rotation *= Quaternion.Euler(0, Lookhorizontal * data.LookSpeed, 0);
+
+                if (Moving)
+                    cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, InstallFOV, data.ReturnSpeedCamera * Time.deltaTime);
+
+        }
+
+    }
+}
